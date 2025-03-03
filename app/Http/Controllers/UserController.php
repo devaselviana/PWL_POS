7b2f4c968a8f1d163fc7c82eb977c879526ea638
devaselviana <selvianadeva8@gmail.com>
2025-03-03 19:13:52 +0700

    P2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,15 @@ class UserController extends Controller
         //UserModel::where('username', 'customer 1')->update($data); //update data user
 
         //coba akses model UserModel
-        $user = UserModel::find(1); //ambil semua data dari tabel m_user
+        //$user = UserModel::find(1); //ambil semua data dari tabel m_user
+        //$user = UserModel::where('level_id', 1)->first();
+        //$user = UserModel::firstWhere('level_id', 1);
+        //$user = UserModel::findOr(1, ['username', 'nama'], function () {
+
+        //});
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
