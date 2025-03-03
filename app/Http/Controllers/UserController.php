7dba77c20b22ffb48d0c71140d158048ef024a2c
devaselviana <selvianadeva8@gmail.com>
2025-03-03 14:46:28 +0700

    manager2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,23 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
         ];
+        UserModel::create($data);
+        //tambah data user dengan Eloquent Model
+        //$data = [
+        //    'nama' => 'Pelanggan Pertama',
+        //];
             /*'username' => 'customer-1',
             'nama' => 'Manager',
             'password' => Hash::make('12345'),
             'level_id' => 4*/
 
-        UserModel::where('username', 'customer 1')->update($data); //update data user
+        //UserModel::where('username', 'customer 1')->update($data); //update data user
 
         //coba akses model UserModel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
