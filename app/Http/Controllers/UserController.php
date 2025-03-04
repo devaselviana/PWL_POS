99479fc2f945a4fd4dc487eca066842a9e273acc
devaselviana <selvianadeva8@gmail.com>
2025-03-04 21:18:47 +0700

    p2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,47 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager56';
+
+        //$user->save();
+
+        //$user->wasChanged(); //true
+        //$user->wasChanged('username'); //true
+        //$user->wasChanged(['username', 'level_id']); //true
+        //$user->wasChanged('nama');//false
+        //dd($user->wasChanged(['nama', 'username'])); //true
+
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean(['nama', 'username']); // false
+
         $user->save();
+
+        $user->isDirty(); // false
+        $user->isClean(); // true
+        dd($user->isDirty());
+        //$user = UserModel::firstOrNew(
+        //    [
+        //        'username' => 'manager33',
+        //        'nama' => 'Manager Tiga Tiga',
+        //        'password' => Hash::make('12345'),
+        //        'level_id' => 2
+        //    ],
+        //);
+        //$user->save();
         //[
         //    'username' => 'manager',
         //]
@@ -31,6 +63,7 @@ class UserController extends Controller
 
         //    ],
         //);
+
         return view('user', ['data' => $user]);
     }
 }
