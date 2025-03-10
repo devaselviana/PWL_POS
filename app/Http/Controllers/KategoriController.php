e95df82170bf4de4819b52b0910cd78b33707c31
devaselviana <selvianadeva8@gmail.com>
2025-03-10 14:18:06 +0700

    jobsheet5P2

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\DataTables\KategoriDataTable;
 use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
+        return $dataTable->render('kategori.index');
         /*$data = [
             'kategori_kode' => 'SNK',
             'kategori_nama' => 'Snack/Makanan Ringan',
@@ -23,8 +25,8 @@ class KategoriController extends Controller
         //$row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
         //return 'Delete data berhasil. Jumlah data yang diupdate: '.$row. ' baris';
 
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+        //$data = DB::table('m_kategori')->get();
+        //return view('kategori', ['data' => $data]);
 
     }
 }
