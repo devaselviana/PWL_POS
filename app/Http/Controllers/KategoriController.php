51482522ebc27d772e0487c09f584c77ae74b7da
devaselviana <selvianadeva8@gmail.com>
2025-03-10 14:40:38 +0700

    jobsheet5P3

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,31 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use App\DataTables\KategoriDataTable;
-use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
     public function index(KategoriDataTable $dataTable)
     {
         return $dataTable->render('kategori.index');
-        /*$data = [
-            'kategori_kode' => 'SNK',
-            'kategori_nama' => 'Snack/Makanan Ringan',
-            'created_at'   => now()
-        ];
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil';*/
+    }
 
-        //$row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        //return 'Update data berhasil. Jumlah data yang diupdate: '.$row. ' baris';
+    public function create()
+    {
+        return view('kategori.create');
+    }
 
-        //$row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        //return 'Delete data berhasil. Jumlah data yang diupdate: '.$row. ' baris';
-
-        //$data = DB::table('m_kategori')->get();
-        //return view('kategori', ['data' => $data]);
-
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori')->with('success', 'Kategori berhasil ditambahkan!');
     }
 }
